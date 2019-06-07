8570b0e27b0971f4f59d9e85274ca658f7a5eb74
Viola <hustlaviola@gmail.com>
2019-06-07 13:01:04 +0100

    Restructure code and add build to run on Heroku

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './server/routes/router';
+import router from './routes/router';
 
 const app = express();
 
--- server/tests/carSpec.js
+++ server/tests/carSpec.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../../app';
+import app from '../app';
 
 chai.use(chaiHttp);
 
