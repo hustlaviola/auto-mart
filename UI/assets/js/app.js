066781a1bdc4b5d171552b8b258519cd7a8a0f38
Viola <hustlaviola@gmail.com>
2019-06-20 10:32:01 +0100

    Bug(ui): fix signup and login links

--- UI/assets/js/app.js
+++ UI/assets/js/app.js
@@ -1,216 +1,147 @@
-const viewBtns = document.querySelectorAll(".view-btn");
-const topModal = document.getElementById("top-modal");
-const modal = document.getElementById("modal");
-const modImage = document.getElementById("mod-image");
-const make = document.getElementById("make");
-const state = document.getElementById("state");
-const status = document.getElementById("status");
-const model = document.getElementById("model");
-const bodyType = document.getElementById("body-type");
-const price = document.getElementById("price");
-const description = document.getElementById("description");
-const topImgs = document.querySelectorAll(".top-img");
-const modFlag = document.getElementById("mod-flag");
-const modEdit = document.getElementById("mod-edit");
-const flagBtn = document.querySelector(".flag-btn");
-const delBtn = document.querySelector(".del-btn");
-const modalForm = document.querySelector(".modal-form");
+/* eslint-disable no-unused-vars */
+const topModal = document.getElementById('top-modal');
+const modal = document.getElementById('modal');
+const modImage = document.getElementById('mod-image');
+const make = document.getElementById('make');
+const state = document.getElementById('state');
+const status = document.getElementById('status');
+const model = document.getElementById('model');
+const bodyType = document.getElementById('body-type');
+const price = document.getElementById('price');
+const description = document.getElementById('description');
+const modFlag = document.getElementById('mod-flag');
+const modEdit = document.getElementById('mod-edit');
+const flagBtn = document.querySelector('.flag-btn');
+const delBtn = document.querySelector('.del-btn');
+const modalForm = document.querySelector('.modal-form');
 
-const reset = () => {
+const resetModalContents = () => {
   if (modFlag) {
-    if (!modFlag.classList.contains("hidden")) {
-      modFlag.classList.add("hidden");
-    }
+    if (!modFlag.classList.contains('hidden')) modFlag.classList.add('hidden');
   }
-  if (!topModal.classList.contains("hidden")) {
-    topModal.classList.add("hidden");
-  }
+  if (!topModal.classList.contains('hidden')) topModal.classList.add('hidden');
   if (flagBtn) {
-    if (!flagBtn.classList.contains("hidden")) {
-      flagBtn.classList.add("hidden");
-    }
+    if (!flagBtn.classList.contains('hidden')) flagBtn.classList.add('hidden');
   }
   if (delBtn) {
-    if (!delBtn.classList.contains("hidden")) {
-      delBtn.classList.add("hidden");
-    }
+    if (!delBtn.classList.contains('hidden')) delBtn.classList.add('hidden');
   }
   if (modalForm) {
-    if (!modalForm.classList.contains("hidden")) {
-      modalForm.classList.add("hidden");
-    }
+    if (!modalForm.classList.contains('hidden')) modalForm.classList.add('hidden');
   }
   if (modEdit) {
-    if (!modEdit.classList.contains("hidden")) {
-      modEdit.classList.add("hidden");
-    }
+    if (!modEdit.classList.contains('hidden')) modEdit.classList.add('hidden');
   }
-  modImage.src = "";
-  make.innerHTML = "";
-  state.innerHTML = "";
-  if (status) status.innerHTML = "";
-  model.innerHTML = "";
-  bodyType.innerHTML = "";
-  price.innerHTML = "";
-  description.innerHTML = "";
+};
+
+const reset = () => {
+  resetModalContents();
+  modImage.src = '';
+  make.innerHTML = '';
+  state.innerHTML = '';
+  if (status) status.innerHTML = '';
+  model.innerHTML = '';
+  bodyType.innerHTML = '';
+  price.innerHTML = '';
+  description.innerHTML = '';
+};
+
+const preview = event => {
+  reset();
+  modal.style.display = 'flex';
+  const { target } = event;
+  topModal.classList.remove('hidden');
+  const parentCont = target.parentNode.parentNode.id;
+  modImage.src = document.querySelector(`#${parentCont} img`).src;
+  make.innerHTML = document.querySelector(`#${parentCont} .make`).innerHTML;
+  state.innerHTML = document.querySelector(`#${parentCont} .state`).innerHTML;
+  model.innerHTML = document.querySelector(`#${parentCont} .model`).innerHTML;
+  bodyType.innerHTML = document.querySelector(`#${parentCont} .body-type`).innerHTML;
+  price.innerHTML = document.querySelector(`#${parentCont} .price`).innerHTML;
+  description.innerHTML = document.querySelector(`#${parentCont} .describe`).innerHTML;
 };
 
 const view = event => {
-  reset();
-  modal.style.display = "flex";
-  const target = event.target;
-  topModal.classList.remove("hidden");
-  const parentCont = target.parentNode.parentNode.id;
-  modImage.src = document.querySelector("#" + parentCont + " " + "img").src;
-  make.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".make"
-  ).innerHTML;
-  state.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".state"
-  ).innerHTML;
-  model.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".model"
-  ).innerHTML;
-  bodyType.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".body-type"
-  ).innerHTML;
-  price.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".price"
-  ).innerHTML;
-  description.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".describe"
-  ).innerHTML;
-  flagBtn.classList.remove("hidden");
+  preview(event);
+  flagBtn.classList.remove('hidden');
 };
 
 const review = event => {
-  reset();
-  modal.style.display = "flex";
-  const target = event.target;
-  topModal.classList.remove("hidden");
-  const parentCont = target.parentNode.parentNode.id;
-  modImage.src = document.querySelector("#" + parentCont + " " + "img").src;
-  make.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".make"
-  ).innerHTML;
-  state.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".state"
-  ).innerHTML;
-  model.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".model"
-  ).innerHTML;
-  bodyType.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".body-type"
-  ).innerHTML;
-  price.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".price"
-  ).innerHTML;
-  description.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".describe"
-  ).innerHTML;
-  delBtn.classList.remove("hidden");
+  preview(event);
+  delBtn.classList.remove('hidden');
 };
 
 const order = event => {
   reset();
-  modal.style.display = "flex";
-  const target = event.target;
-  topModal.classList.remove("hidden");
+  modal.style.display = 'flex';
+  const { target } = event;
+  topModal.classList.remove('hidden');
 
   const parentCont = target.parentNode.parentNode.id;
-  modImage.src = document.querySelector("#" + parentCont + " " + "img").src;
-  make.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".make"
-  ).innerHTML;
-  state.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".state"
-  ).innerHTML;
-  model.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".model"
-  ).innerHTML;
-  bodyType.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".body-type"
-  ).innerHTML;
-  price.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".price"
-  ).innerHTML;
-  modalForm.classList.remove("hidden");
+  modImage.src = document.querySelector(`#${parentCont} img`).src;
+  make.innerHTML = document.querySelector(`#${parentCont} .make`).innerHTML;
+  state.innerHTML = document.querySelector(`#${parentCont} .state`).innerHTML;
+  model.innerHTML = document.querySelector(`#${parentCont} .model`).innerHTML;
+  bodyType.innerHTML = document.querySelector(`#${parentCont} .body-type`).innerHTML;
+  price.innerHTML = document.querySelector(`#${parentCont} .price`).innerHTML;
+  modalForm.classList.remove('hidden');
 };
 
 const review2 = event => {
   reset();
-  modal.style.display = "flex";
-  const target = event.target;
-  topModal.classList.remove("hidden");
+  modal.style.display = 'flex';
+  const { target } = event;
+  topModal.classList.remove('hidden');
   const parentCont = target.parentNode.parentNode.id;
-  modImage.src = document.querySelector("#" + parentCont + " " + "img").src;
-  make.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".make"
-  ).innerHTML;
-  state.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".state"
-  ).innerHTML;
-  model.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".model"
-  ).innerHTML;
-  status.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".status"
-  ).innerHTML;
-  bodyType.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".body-type"
-  ).innerHTML;
-  price.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".price"
-  ).innerHTML;
-  description.innerHTML = document.querySelector(
-    "#" + parentCont + " " + ".describe"
-  ).innerHTML;
-  if (delBtn) {
-    delBtn.classList.remove("hidden");
-  }
+  modImage.src = document.querySelector(`#${parentCont} img`).src;
+  make.innerHTML = document.querySelector(`#${parentCont} .make`).innerHTML;
+  state.innerHTML = document.querySelector(`#${parentCont} .state`).innerHTML;
+  model.innerHTML = document.querySelector(`#${parentCont} .model`).innerHTML;
+  status.innerHTML = document.querySelector(`#${parentCont} .status`).innerHTML;
+  bodyType.innerHTML = document.querySelector(`#${parentCont} .body-type`).innerHTML;
+  price.innerHTML = document.querySelector(`#${parentCont} .price`).innerHTML;
+  description.innerHTML = document.querySelector(`#${parentCont} .describe`).innerHTML;
+  if (delBtn) delBtn.classList.remove('hidden');
 };
 
-const closeModal = document.getElementById("close-modal");
+const closeModal = document.getElementById('close-modal');
 
-closeModal.onclick = () => {
-  modal.style.display = "none";
-};
+if (closeModal) {
+  closeModal.addEventListener('click', event => {
+    modal.style.display = 'none';
+  });
+}
 
 const flag = () => {
-  topModal.classList.add("hidden");
-  modFlag.classList.remove("hidden");
+  topModal.classList.add('hidden');
+  modFlag.classList.remove('hidden');
 };
 
 const edit = () => {
   reset();
-  modal.style.display = "flex";
-  modEdit.classList.remove("hidden");
+  modal.style.display = 'flex';
+  modEdit.classList.remove('hidden');
 };
 
 const openMyAds = () => {
-  location.href = "myAds.html";
+  window.location.href = 'myAds.html';
+};
+
+const viewAds = event => {
+  event.preventDefault();
+  window.location.href = 'viewAds.html';
 };
 
 const openOrders = () => {
-  location.href = "orders.html";
+  window.location.href = 'orders.html';
 };
 
-const adBtns = document.querySelectorAll(".ad-btn");
-const modal2 = document.getElementById("modal2");
+const login = event => {
+  event.preventDefault();
+  window.location.href = 'home.html';
+};
 
-adBtns.forEach(adBtn => {
-  adBtn.addEventListener("click", () => {
-    modal2.style.display = "block";
-  });
-});
-const imgs = document.querySelectorAll(".ad-img");
-const modalImg = document.getElementById("mod-img");
-const caption = document.getElementById("caption");
-
-imgs.forEach(img => {
-  img.addEventListener("click", () => {
-    modal.style.display = "block";
-    modalImg.src = img.src;
-    caption.innerHTML = img.alt;
-  });
-});
+const signUp = event => {
+  event.preventDefault();
+  window.location.href = 'login.html';
+};
