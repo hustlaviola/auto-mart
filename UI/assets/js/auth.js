c77958d8ffeee027a408b0dc46c0adea7cea3fe5
Viola <hustlaviola@gmail.com>
2019-07-15 06:34:50 +0100

    Feat(fetch-api): consume signup and login endpoint
    Install cors
    Update the server file to use cors
    Add a gif file to display while fetching from database
    Update css to set the gif file display to none
    Update landing page to display error message and gif file
    Update login page to display error message and gif file
    Create a new file to consume signup and login endpoint
    
    [Finishes #167270309]

---
+++ UI/assets/js/auth.js
@@ -0,0 +1,61 @@
+const spin = document.querySelector('.spin');
+const errorMessage = document.querySelector('.error');
+
+const authenticateUser = (userInfo, endpoint) => {
+  const url = `https://automobilemart.herokuapp.com/api/v1/auth/${endpoint}`;
+  spin.style.display = 'flex';
+  errorMessage.textContent = '';
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userInfo),
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'success') {
+        spin.style.display = 'none';
+        const { token } = data.data[0];
+        localStorage.setItem('token', token);
+        window.location.href = window.location.href.includes('index')
+          ? './login.html' : './home.html';
+      } else {
+        spin.style.display = 'none';
+        errorMessage.textContent = data.error;
+      }
+    })
+    .catch(error => {
+      throw error;
+    });
+};
+
+document.querySelector('.auth-form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  let endpoint = 'login';
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const userDetails = {
+    email, password,
+  };
+
+  if (window.location.href.includes('index')) {
+    endpoint = 'signup';
+
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const confirmpassword = document.getElementById('confirmpassword').value;
+    const address = document.getElementById('address').value;
+
+    if (password !== confirmpassword) {
+      errorMessage.textContent = 'password does not match';
+      return;
+    }
+    Object.assign(userDetails, {
+      firstname, lastname, address,
+    });
+  }
+
+  authenticateUser(userDetails, endpoint);
+});
